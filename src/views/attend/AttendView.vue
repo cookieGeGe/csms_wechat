8f176fcacdc8bce61986c86ae5e211c9b7d56ae8
mushudao <1659646607@qq.com>
2019-12-18 23:55:07 +0800

    修改progress

--- src/views/attend/AttendView.vue
+++ src/views/attend/AttendView.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="van-content">
-    <Calendar
+    <Calendar ref="Calendar"
       @choseDay="clickDay"
       @changeMonth="changeDate"
+      :markDateMore="nolist"
       >
-     <!-- // v-on:isToday="clickToday"
-      // :markDate=arr // arr=['2018/4/1','2018/4/3'] 标记4月1日和4月3日 简单标记
-      //:markDateMore=arr // 多种不同的标记
-      // 第一个标记和第二个标记不能同时使用
-      // :agoDayHide='1514937600' //某个日期以前的不允许点击  时间戳10位
-      // :futureDayHide='1525104000' //某个日期以后的不允许点击  时间戳10位
-      // :sundayStart="true" //默认是周一开始 当是true的时候 是周日开始-->
     </Calendar>
-
     <div class="card">
-      <AttendItem :isReload="isReload" :result="result[0]"></AttendItem>
+      <AttendItem :isReload="isReload" :result="result[index]"></AttendItem>
     </div>
+
+    <div class="attend" >
+      <h3 style="font-size: 1.4rem">{{dayData.year}}-{{dayData.month}}-{{dayData.day}} 考勤明细</h3>
+      <van-row type="flex" align="center" >
+        <van-col span="3" class="t-center"><div>上</div>午</van-col>
+        <van-col>
+          <div class="time">上班: {{dayData.amin}}</div>
+          <div>打卡地址:{{dayData.aminpos}}</div>
+          <div class="time">下班: {{dayData.amout}}</div>
+          <div>打卡地址:{{dayData.amoutpos}}</div>
+        </van-col>
+      </van-row>
+      <van-row type="flex" align="center">
+        <van-col span="3"  class="t-center"><div>下</div>午</van-col>
+        <van-col>
+          <div class="time">上班: {{dayData.pmin}}</div>
+          <div>打卡地址:{{dayData.pmoutpos}}</div>
+          <div class="time">下班: {{dayData.pmout}}</div>
+          <div >打卡地址:{{dayData.pmoutpos}}</div>
+        </van-col>
+      </van-row>
+    </div>
+
   </div>
 </template>
 
@@ -29,34 +45,57 @@
     data() {
       return {
         searchType: {
-          id: -1,
+          id:55,
+          year:2019,
+          month:7,
         },
         isReload: true,
         result: [],
+        nolist:[],
+        index:0,
+        dayData:{},
       }
     },
     components: {
       Calendar,
       AttendItem,
-
     },
     methods: {
       clickDay(data) {
-        console.log(data); //选中某天
+        var day=data.split('/')[2];
+        this.result.forEach((val,index,arr)=>{
+          if(parseInt(val.day)==day){
+            this.dayData=val;
+            this.index=index;
+          }
+        });
       },
       changeDate(data) {
-        console.log(data); //左右点击切换月份
-      },
-      clickToday(data) {
-        console.log(data); //跳到了本月
+        if(!this.changeDates){
+            return
+        }
+        var arrDate=data.split('/');
+        this.searchType.year=arrDate[0];
+        this.searchType.month=arrDate[1];
+        this.reload();
       },
       reload() {
-
-        queryAttendInfo({id:55,year:2019,month:7,}).then((res) => {
+        queryAttendInfo(this.searchType).then((res) => {
+          this.changeDates=false;
+          this.$refs.Calendar.ChoseMonth(this.searchType.year+'-'+this.searchType.month,false);
+          setTimeout(()=>{
+            this.changeDates=true
+          },500);
           var ret = res.data;
-          console.log(res);
           this.isReload = false; //是否重新赋值
           this.result = ret;
+          ret.forEach((val,index,arr)=>{
+            if(val.miss==='缺考'){
+                this.nolist.push({date:val.year+'/'+val.month+'/'+val.day,className:"mark1"})
+            }else{
+               this.nolist.push({date:val.year+'/'+val.month+'/'+val.day,className:"mark2"})
+            }
+          });
         });
       },
       showimg(img) {
@@ -66,8 +105,11 @@
 
     },
     mounted() {
-      this.searchType.id = this.$route.params.id;
       this.reload();
+
+
+     // this.searchType= this.$route.params;
+      //this.reload();
     }
   }
 </script>
@@ -77,6 +119,22 @@
     padding: 0.5rem 0.5rem;
     font-size: 1.1rem !important;
   }
+  .attend /deep/ .van-row{
+      border-bottom: 1px solid #ececec;
+     padding-bottom: 1rem;
+  }
+  .attend .t-center{
+    text-align: center;
+    margin-right: 10px;
+    font-size: 1.4rem;
+  }
+  .time{
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: .5rem 0;
+  }
+
+
   .van-content /deep/ .wh_content_all{
     background-color: rgba(255, 255, 255, 0.77);
     font-size: 1.2rem;
@@ -100,7 +158,23 @@
   .van-content /deep/ .wh_item_date{
     color: #3c3c3c;
   }
+
   .van-content /deep/ .wh_item_date.wh_other_dayhide{
     color: #bfbfbf;
   }
+  .van-content /deep/ .wh_item_date.wh_isToday{
+    background: #8eaccc;
+  }
+  .van-content /deep/ .wh_item_date.wh_chose_day{
+    background: #8eaccc!important;
+  }
+  .van-content /deep/ .wh_item_date.mark1{
+    background: #d9aa60;
+    border-radius: 100px;
+  }
+  .van-content /deep/ .wh_item_date.mark2{
+    background: #8ec5cc;
+    border-radius: 100px;
+  }
+
 </style>
