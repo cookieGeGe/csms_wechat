83bfe3525594937aba425a19bb375d7a98f4e20b
zhang <zhang@qq.com>
2020-01-04 15:48:32 +0800

    添加预览pdf

--- src/views/help/viewPDF.vue
+++ src/views/help/viewPDF.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    {{currentPage}} / {{pageCount}}
-    <pdf
-      src="./assets/use.pdf"
-      @num-pages="pageCount = $event"
-      @page-loaded="currentPage = $event"
-    ></pdf>
+  <div class="pdf" v-show="fileType === 'pdf'">
+    <p class="arrow">
+      <span @click="changePdfPage(0)" class="turn" :class="{grey: currentPage==1}">Preview</span>
+      {{currentPage}} / {{pageCount}}
+      <span @click="changePdfPage(1)" class="turn" :class="{grey: currentPage==pageCount}">Next</span>
+    </p>
+    <pdf style="width: 100%"
+      :src="src"
+      :page="currentPage"
+      @num-pages="pageCount=$event"
+      @page-loaded="currentPage=$event"
+      @loaded="loadPdfHandler"
+    >
+    </pdf>
   </div>
 </template>
 
@@ -19,10 +26,50 @@
     },
     data() {
       return {
-        currentPage: 0,
-        pageCount: 0,
+        currentPage: 0, // pdf文件页码
+        pageCount: 0, // pdf文件总页数
+        fileType: 'pdf', // 文件类型
+        src: './static/use.pdf', // pdf文件地址
       }
+    },
+    created() {
+      // 有时PDF文件地址会出现跨域的情况,这里最好处理一下
+      this.src = pdf.createLoadingTask(this.src)
+    },
+    methods: {
+      // 改变PDF页码,val传过来区分上一页下一页的值,0上一页,1下一页
+      changePdfPage(val) {
+        // console.log(val)
+        if (val === 0 && this.currentPage > 1) {
+          this.currentPage--
+          // console.log(this.currentPage)
+        }
+        if (val === 1 && this.currentPage < this.pageCount) {
+          this.currentPage++
+          // console.log(this.currentPage)
+        }
+      },
+
+      // pdf加载时
+      loadPdfHandler(e) {
+        this.currentPage = 1 // 加载的时候先加载第一页
+      }
+
     }
   }
 
 </script>
+
+<style scoped>
+  .pdf {
+    margin: 0 auto;
+  }
+
+  .main {
+    position: relative;
+    margin: 0 auto;
+    width: 50%;
+    height: 800px;
+    overflow: scroll;
+  }
+</style>
